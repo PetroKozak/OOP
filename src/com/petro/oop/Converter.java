fa70c161e7f5054acd03a84d99bc67aec9604fa2
PetroKozak <kozak.petya@gmail.com>
2021-01-09 11:31:53 +0100

    Initial Commit

--- src/com/petro/oop/Converter.java
+++ src/com/petro/oop/Converter.java
@@ -1,30 +1,43 @@
 package com.petro.oop;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class Converter {
-    public void convert() {
-        HashMap<Integer, BasicConvert> systems = new HashMap<>();
-        Scanner sc = new Scanner(System.in);
+    void result(int system, long number, BasicConvert base) {
+        System.out.println("Your convert system is" + " " + system);
+        System.out.println("Your wished number is" + " " + number);
+        System.out.print("Your result is: ");
+        System.out.println(base.convertingProcess(number));
+    }
 
-        systems.put(2, new BinaryConvert());
-        systems.put(8, new OctalConvert());
-        systems.put(10, new DecimalConvert());
-        systems.put(16, new HexadecimalConvert());
+    public void convert() {
+        BasicConvert convert;
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter Converting system:");
         int convertSystem = sc.nextInt();
         System.out.println("Please enter number to convert:");
         long wishedNumber = sc.nextLong();
 
-        if (systems.containsKey(convertSystem)) {
-            System.out.println("Your convert system is" + " " + convertSystem);
-            System.out.println("Your wished number is" + " " + wishedNumber);
-            System.out.print("Your result is: ");
-            System.out.println(systems.get(convertSystem).convertingProcess(wishedNumber));
-        } else {
-            System.out.println("You set wrong convert System, please try again");
+        switch (convertSystem) {
+            case (2):
+                convert = new BinaryConvert();
+                result(convertSystem, wishedNumber, convert);
+                break;
+            case (8):
+                convert = new OctalConvert();
+                result(convertSystem, wishedNumber, convert);
+                break;
+            case (10):
+                convert = new DecimalConvert();
+                result(convertSystem, wishedNumber, convert);
+                break;
+            case (16):
+                convert = new HexadecimalConvert();
+                result(convertSystem, wishedNumber, convert);
+                break;
+            default:
+                System.out.println("You've set wrong converting system");
         }
 
     }
